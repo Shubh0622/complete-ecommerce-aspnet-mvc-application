89d5f376a00e8986d15afc14df84c2d837110842
shubh <shubhambachhav8@gmail.com>
2024-02-24 14:20:33 +0530

    first commit

--- eTicket/Program.cs
+++ eTicket/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+builder.Services.AddDbContext<eTicket.Data.AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
