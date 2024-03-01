d2ae848dba1eade4ac9ba6b2932b2d2664ccb25a
shubh <shubhambachhav8@gmail.com>
2024-03-01 15:18:50 +0530

    Models and Controller added

--- eTicket/Models/Movie.cs
+++ eTicket/Models/Movie.cs
@@ -10,7 +10,7 @@ namespace eTicket.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public double price { get; set; }
+        public double Price { get; set; }
         public string ImageURL { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
--- eTicket/Program.cs
+++ eTicket/Program.cs
@@ -1,14 +1,16 @@
+using eTicket.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<eTicket.Data.AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnectionString")
+    ));
 
 var app = builder.Build();
-builder.Services.AddDbContext<eTicket.Data.AppDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,6 +19,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//Seed Database
+AppDbInitializer.Seed(app);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
