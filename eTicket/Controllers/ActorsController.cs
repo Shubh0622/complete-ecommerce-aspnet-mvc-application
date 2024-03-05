e8aeb77cc6259a849178f5659c68628bc10dde67
shubh <shubhambachhav8@gmail.com>
2024-03-05 11:53:34 +0530

    basic view changes

--- eTicket/Controllers/ActorsController.cs
+++ eTicket/Controllers/ActorsController.cs
@@ -14,7 +14,7 @@ namespace eTicket.Controllers
         public IActionResult Index()
         {
             var data = _context.Actors.ToList();
-            return View();
+            return View(data);
         }
     }
 }
--- eTicket/Controllers/MoviesController.cs
+++ eTicket/Controllers/MoviesController.cs
@@ -13,8 +13,8 @@ namespace eTicket.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.ToListAsync();
-            return View();
+            var allMovies = await _context.Movies.Include(n=>n.Cinema).OrderBy(n=>n.Name).ToListAsync();
+            return View(allMovies);
         }
     }
 }
